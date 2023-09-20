282489e73bf474b10c7f1c046811bd5cb6f0f979
nntvi <nntvi.2609@gmail.com>
2023-09-20 14:37:03 +0700

    connect mongo compass

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use('/users', usersRouter)
+
+databaseService.connect() // sử dụng với class
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ src/services/database.services.ts
@@ -0,0 +1,25 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { config } from 'dotenv'
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@startwithmongodb.p65hxjm.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseService {
+  private client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
+  }
+}
+// Tạo 1 object từ class DatabaseService
+const databaseService = new DatabaseService()
+export default databaseService
