50a5d6c15bb3f3b2e03aaaf3508b6fe83da9cdf1
nntvi <nntvi.2609@gmail.com>
2024-01-25 15:06:56 +0700

    user message error

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,3 +1,25 @@
 export const userMessageError = {
-  VALIDATION_ERROR: 'VALIDATION_ERROR'
+  VALIDATION_ERROR: 'Validation error',
+  USER_NOT_FOUND: 'User not found',
+
+  NAME_REQUIRED: 'Name is required',
+  NAME_IS_STRING: 'Name must be a string',
+  NAME_LENGTH: 'Name must be at least 3 characters',
+
+  EMAIL_ALREADY_EXIST: 'Email already exists',
+  EMAIL_REQUIRED: 'Email is required',
+  EMAIL_INVALID: 'Email is not valid',
+
+  PASSWORD_REQUIRED: 'Password is required',
+  PASSWORD_LENGTH: 'Password must be at least 6 characters',
+  PASSWORD_MUST_BE_STRONG:
+    'Password must be from 6 to 50 characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+  PASSWORD_STRING: 'Password must be a string',
+  CONFIRM_PASSWORD_REQUIRED: 'Confirm password is required',
+  CONFIRM_PASSWORD_LENGTH: 'Confirm password must be at least 6 characters',
+  CONFIRM_PASSWORD_MUST_BE_STRONG:
+    'Confirm password must be from 6 to 50 characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+  CONFIRM_PASSWORD_MUST_MATCH: 'Confirm password must match with password',
+
+  DATE_OF_BIRTH_IS_ISO8601: 'Date of birth must be in ISO 8601 format'
 } as const
--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,7 +1,6 @@
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { checkSchema } from 'express-validator'
-import httpStatus from '~/constants/httpStatus'
-import { ErrorWithStatus } from '~/models/Error'
+import { userMessageError } from '~/constants/messages'
 import userService from '~/services/users.services'
 import { validate } from '~/utils/validation'
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
@@ -15,53 +14,67 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
 export const registerValidator = validate(
   checkSchema({
     name: {
-      notEmpty: true,
-      isString: true,
+      notEmpty: {
+        errorMessage: userMessageError.NAME_REQUIRED
+      },
+      isString: {
+        errorMessage: userMessageError.NAME_IS_STRING
+      },
       isLength: {
         options: { min: 3, max: 50 },
-        errorMessage: 'Name must be at least 3 characters'
+        errorMessage: userMessageError.NAME_LENGTH
       },
       trim: true
     },
     email: {
-      notEmpty: true,
-      isEmail: true,
+      notEmpty: {
+        errorMessage: userMessageError.EMAIL_REQUIRED
+      },
+      isEmail: {
+        errorMessage: userMessageError.EMAIL_INVALID
+      },
       trim: true,
-      errorMessage: 'Email is not valid',
       custom: {
         options: async (value) => {
           const user = await userService.checkEmailExist(value)
           if (user) {
-            throw new Error('Email already exists')
+            throw new Error(userMessageError.EMAIL_ALREADY_EXIST)
           }
           return true
         }
       }
     },
     password: {
-      notEmpty: true,
-      isString: true,
+      notEmpty: {
+        errorMessage: userMessageError.PASSWORD_REQUIRED
+      },
+      isString: {
+        errorMessage: userMessageError.PASSWORD_STRING
+      },
       isLength: {
-        options: { min: 6, max: 50 }
+        options: { min: 6, max: 50 },
+        errorMessage: userMessageError.PASSWORD_LENGTH
       },
       isStrongPassword: {
-        errorMessage: 'Mật khẩu không hợp lệ',
+        errorMessage: userMessageError.PASSWORD_MUST_BE_STRONG,
         options: { minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }
       }
     },
     confirm_password: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: userMessageError.CONFIRM_PASSWORD_REQUIRED
+      },
       isLength: {
         options: { min: 6, max: 50 }
       },
       isStrongPassword: {
-        errorMessage: 'Mật khẩu không hợp lệ',
+        errorMessage: userMessageError.CONFIRM_PASSWORD_MUST_BE_STRONG,
         options: { minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }
       },
       custom: {
         options: (value, { req }) => {
           if (value !== req.body.password) {
-            throw new Error('Mật khẩu xác nhận không khớp')
+            throw new Error(userMessageError.CONFIRM_PASSWORD_MUST_MATCH)
           }
           return true
         }
@@ -73,7 +86,8 @@ export const registerValidator = validate(
         options: {
           strict: true, // phải tuân theo ISO 8601 chặt chẽ, ko chấp nhận các biến thể ko chính thống của định dạng ngày tháng năm
           strictSeparator: true // các ký tự ngăn cách trong ngày tháng năm (như dấu -, /) phải đúng quy định
-        }
+        },
+        errorMessage: userMessageError.DATE_OF_BIRTH_IS_ISO8601
       }
     }
   })
