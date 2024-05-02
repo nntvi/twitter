3c07ee0bfcf7ab47adffc3ca7dd1ba36b595137d
nntvi <vietnamblockchain.asia>
2024-05-02 10:44:10 +0700

    fix bug content range

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -46,10 +46,10 @@ export const serverVideoStreamController = (req: Request, res: Response) => {
   // Lấy giá trị bytes bắt đầu từ header Range
   const start = Number(range.replace(/\D/g, ''))
   // Lấy giá trị bytes kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize
-  const end = Math.min(start + chunkSize, videoSize)
+  const end = Math.min(start + chunkSize, videoSize - 1)
   // Dung lượng thực tế cho mỗi đoạn video stream
   // thường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
-  const contentLength = end - start
+  const contentLength = end - start + 1
   const contentType = mime.getType(videoPath) || 'video/*'
   const headers = {
     'Content-Range': `bytes ${start}-${end}/${videoSize}`,
@@ -61,3 +61,22 @@ export const serverVideoStreamController = (req: Request, res: Response) => {
   const videoStream = fs.createReadStream(videoPath, { start, end })
   videoStream.pipe(res)
 }
+
+/**
+ * Format của header Content-range: bytes <start>-<end>/<videoSize>
+ * Ví dụ: Content-range: bytes 1084567-3145727/3145728
+ * Yêu cầu là 'end' phải luôn nhỏ hơn videSize
+ * 'Content-range': 'bytes 0-100/100 => là sai
+ * 'Content-range': 'bytes 0-99/100 => là đúng
+ *
+ * Còn Content-Length sẽ là end - start + 1. Đại diện cho khoảng cách
+ * Để dễ hình dung, cứ tưởng tượng từ 0 -> 10 có 11 số
+ * byte cũng tương tự, start = 0, end = 10 => 11 byte
+ * Công thức là end - start + 1
+ *
+ * ChunkSize = 50
+ * VideoSize = 100
+ * |0-----------------50| |51-----------------99|100 (end)
+ * stream 1: start = 0, end = 50, contentLength = 51
+ * stream 2: start = 51, end = 99, contentLength = 49
+ */
