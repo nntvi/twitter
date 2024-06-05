9e97beb75c9b45d886c3c26751db3fc750915826
nntvi <vietnamblockchain.asia>
2024-06-05 17:06:28 +0700

    schema tweet

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -22,3 +22,15 @@ export enum EncodingStatus {
   Success,
   Failed
 }
+
+export enum TweetType {
+  Tweet,
+  Retweet,
+  Comment,
+  QuoteTweet
+}
+
+export enum TweetAudience {
+  Everyone,
+  TwitterCircle
+}
